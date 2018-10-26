788c5bc9791929d1013759cef9244da46a6906b9
ArimaAnthony <32500201+ArimaAnthony@users.noreply.github.com>
2018-10-26 14:38:34 -0700

    Add files via upload

---
+++ get_stats.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+
+if ((scalar @ARGV) != 1) {print "please provide the <bamfile>\n"; exit;}
+open (FILE,"/projects/arima/Projects/RefFiles/software/samtools-0.1.18/samtools view $ARGV[0] |"); 
+
+my $all; my $intra; my $inter; 
+my $intra_1; my $intra_10; my $intra_15; my $intra_20;
+while (my $line = <FILE>)
+{ chomp $line;
+  my @liner = split('\t',$line);
+  $all++;
+
+  if ($liner[6] eq '=')
+  {$intra++; 
+   if (abs($liner[8]) >= 1000)  {$intra_1++;}
+   if (abs($liner[8]) >= 10000)  {$intra_10++;}
+   if (abs($liner[8]) >= 15000) {$intra_15++;}
+   if (abs($liner[8]) >= 20000) {$intra_20++;}
+  }
+  else {$inter++;}
+
+}
+close FILE;
+
+$all      = $all/2;
+$intra    = $intra/2; 
+$intra_1  = $intra_1/2;  
+$intra_10  = $intra_10/2;  
+$intra_15 = $intra_15/2;
+$intra_20 = $intra_20/2;  
+$inter    = $inter/2;
+
+print "All            $all\n";
+print "All intra      $intra\n";
+print "All intra 1kb  $intra_1\n";
+print "All intra 10kb $intra_10\n";
+print "All intra 15kb $intra_15\n";
+print "All intra 20kb $intra_20\n";
+print "All inter      $inter\n";
